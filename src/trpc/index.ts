cb2536dbe538bb3131346cbaed8364fe5fc99678
unknown <saummanzoor49@gmail.com>
2023-11-13 22:47:31 +0500

    db-setup

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,21 +1,34 @@
-import { publicProcedure, router } from './trpc';
-import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
-import {TRPCError} from '@trpc/server'
+import { db } from "@/db";
+import { publicProcedure, router } from "./trpc";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { TRPCError } from "@trpc/server";
 
 export const appRouter = router({
-  authCallback:publicProcedure.query(async()=>{
-    const {getUser} = getKindeServerSession()
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession()
     const user = await getUser()
-    if(!user||!user.id) throw new TRPCError({code:'UNAUTHORIZED'})
-
+    if (!user || !user.id||!user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
     // check if user is in DB
 
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
 
-    return {success:true}
-  })
+    if(!dbUser){
+      // 
+      await db.user.create({
+        data:{
+          id:user.id,
+          email:user.email
+        }
+      })
+    }
+    return { success: true };
+  }),
 });
- 
 // Export type router type signature,
 // NOT the router itself.
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
