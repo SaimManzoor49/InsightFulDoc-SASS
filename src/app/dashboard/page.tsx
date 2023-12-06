e83085d9752df18c3c37cee297351620131f52bb
unknown <saummanzoor49@gmail.com>
2023-12-06 20:52:25 +0500

    bug-fixing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,9 +4,9 @@ import { redirect } from 'next/navigation';
 export default async function Dashboard() {
     const {getUser} = getKindeServerSession();
     const user =  await getUser();
-
-    if(!user||!user.id) redirect('/auth-callback?origin=dashboard')
-
+console.log(user);
+    // if(!user||!user.id) redirect('/auth-callback?origin=dashboard')
+  
 
   return (
     <div></div>
