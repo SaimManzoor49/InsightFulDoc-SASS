90b6605cb9dc9c6d97d5105f315e16a716e5aa49
unknown <saummanzoor49@gmail.com>
2023-12-07 15:25:02 +0500

    dashboard-schema-update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation';
-export default async function Dashboard() {
+import { db } from '@/db';
+import Dashboard from '@/components/Dashboard';
+export default async function Page() {
 
     const {getUser} = getKindeServerSession();
     const user =  await getUser();
+    
 
     if(!user||!user.id) redirect('/auth-callback?origin=dashboard')
 
-
+    const dbUser = await db.user.findUnique({
+      where:{
+        id:user.id
+      }
+    })
+if(!dbUser) redirect('/auth-callback?origin=dashboard')
   return (
-    <div></div>
+   <Dashboard />
   )
 }
