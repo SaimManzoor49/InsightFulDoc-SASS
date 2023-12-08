c75a58dd768ff37655275eb4aa4929e836836794
unknown <saummanzoor49@gmail.com>
2023-12-08 22:05:17 +0500

    scalling-func

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -4,35 +4,36 @@ import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 import { Document, Page, pdfjs } from 'react-pdf'
 import { useResizeDetector } from 'react-resize-detector'
+import Topbar from './Topbar'
+import SimpleBar from 'simplebar-react'
 
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
-import Topbar from './Topbar'
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 
 const PdfRenderer = ({ url }: { url: string }) => {
-const [numPages, setNumPages] = useState<number>()
-const [currPage, setcurrPage] = useState(1)
+  const [numPages, setNumPages] = useState<number>()
+  const [currPage, setcurrPage] = useState(1)
+  const [scale,setScale] = useState<number>(1)
+  
 
   const { width, ref } = useResizeDetector()
 
   return (
     <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
-      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="flex items-center gap-1.5">
-          <Topbar numPages={numPages as number} setcurrPage={setcurrPage} currPage={currPage} />
-        </div>
-      </div>
+      <Topbar numPages={numPages as number} setcurrPage={setcurrPage} currPage={currPage} scale={scale} setScale={setScale}/>
       <div className="flex-1 w-full max-h-screen">
+        <SimpleBar autoHide={false} className='max-h-[cal(100vh-10rem)]' >
         <div className="" ref={ref}>
           <Document
             file={url}
             className={'max-h-full'}
             onLoadError={() => { toast.error("Error while loading PDF") }}
             loading={<div className='flex justify-center'><Loader2 className='my-24 h-6 w-4 animate-spin' /></div>}
-            onLoadSuccess={({numPages})=>{setNumPages(numPages)}}
+            onLoadSuccess={({ numPages }) => { setNumPages(numPages) }}
           >
             {/* {width ?
               (
@@ -47,11 +48,13 @@ const [currPage, setcurrPage] = useState(1)
               )
             } */}
             <Page
-              width={width?width:1}
+              width={width ? width : 1}
               pageNumber={currPage}
+              scale={scale}
             />
           </Document>
         </div>
+        </SimpleBar>
       </div>
     </div>
   )
--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,32 +1,109 @@
 'use client'
 import React, { Dispatch, SetStateAction, useState } from 'react'
 import { Button } from './ui/button'
-import { ChevronDown, ChevronUp } from 'lucide-react'
+import { ChevronDown, ChevronUp, Search } from 'lucide-react'
 import { Input } from './ui/input'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 
-const Topbar = ({ numPages, currPage,setcurrPage }: { numPages: number, currPage:number,setcurrPage: Dispatch<SetStateAction<number>> }) => {
+import { zodResolver } from '@hookform/resolvers/zod'
+import { cn } from '@/lib/utils'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from './ui/dropdown-menu'
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+
+const Topbar = ({ numPages, currPage, setcurrPage,scale, setScale }: { numPages: number, currPage: number, setcurrPage: Dispatch<SetStateAction<number>>, scale:number, setScale:Dispatch<SetStateAction<number>> }) => {
+
+    const validator = z.object({
+        page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages!)
+    })
+
+    type TValidator = z.infer<typeof validator>
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue
+    } = useForm<TValidator>({
+        defaultValues: {
+            page: "1"
+        },
+        resolver: zodResolver(validator)
+    })
+
+    const handlePageSubmit = ({ page }: TValidator) => {
+        setcurrPage(Number(page))
+        setValue("page", String(page))
+    }
     return (
-        <>
-            <Button aria-label='previous page' variant={'ghost'} disabled={currPage<=1}
-                onClick={() => {
-                    setcurrPage((s) => (
-                        s - 1 > 1 ? s - 1 : 1
-                    ))
-                }}
-            >
-                <ChevronDown className='h-4 w-4' />
-            </Button>
+        <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
             <div className="flex items-center gap-1.5">
-                <Input className='w-12 h-8' />
-                <p className='text-zinc-700 text-sm space-x-1'>
-                    <span>/</span>
-                    <span>{numPages ?? "X"}</span>
-                </p>
+                <Button aria-label='previous page' variant={'ghost'} disabled={currPage <= 1}
+                    onClick={() => {
+                        setcurrPage((s) => (
+                            s - 1 > 1 ? s - 1 : 1
+                        ))
+                    }}
+                >
+                    <ChevronDown className='h-4 w-4' />
+                </Button>
+                <div className="flex items-center gap-1.5">
+                    <Input {...register("page")} // a bug
+                     className={cn(
+                        "w-12 h-8",
+                        errors.page && "focus-visible:ring-red-500"
+                    )} onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            handleSubmit(handlePageSubmit)()
+                        }
+                    }} />
+                    <p className='text-zinc-700 text-sm space-x-1'>
+                        <span>/</span>
+                        <span>{numPages ?? "X"}</span>
+                    </p>
+                </div>
+                <Button aria-label='next page' variant={'ghost'}
+                    disabled={numPages === undefined || currPage === numPages}
+                    onClick={() => {
+                        setcurrPage(s => s + 1 > numPages! ? numPages! : s + 1)
+                    }}
+                >
+                    <ChevronUp className='h-4 w-4' />
+                </Button>
             </div>
-            <Button aria-label='next page' variant={'ghost'}>
-                <ChevronUp className='h-4 w-4' />
-            </Button>
-        </>
+            <div className="space-x-2">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button className='gap-1.5' aria-label='zoom' variant={'ghost'} >
+                            <Search className='h-4 w-4'/>
+                            {scale*100}% <ChevronDown className='h-3 w-3 opacity-50' />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem onSelect={()=>{
+                            setScale(1)
+                        }}>
+                            100%
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={()=>{
+                            setScale(1.5)
+                        }}>
+                            150%
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={()=>{
+                            setScale(2)
+                        }}>
+                            200%
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={()=>{
+                            setScale(2.5)
+                        }}>
+                            250%
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </div>
     )
 }
 
