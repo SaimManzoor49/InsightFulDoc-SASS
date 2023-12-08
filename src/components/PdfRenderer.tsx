e16b4ad271e2b8a9de070af7d8562bd2040a480d
unknown <saummanzoor49@gmail.com>
2023-12-08 22:46:58 +0500

    pdf-renderer-complete

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -9,6 +9,7 @@ import SimpleBar from 'simplebar-react'
 
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
+import { cn } from '@/lib/utils'
 
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
@@ -17,35 +18,37 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 const PdfRenderer = ({ url }: { url: string }) => {
   const [numPages, setNumPages] = useState<number>()
   const [currPage, setcurrPage] = useState(1)
-  const [scale,setScale] = useState<number>(1)
+  const [scale, setScale] = useState<number>(1)
   const [rotation, setRotation] = useState(0)
-  
+  const [renderedScale, setRenderedScale] = useState<number | null>(null)
+
+  const isLoading = renderedScale !== scale
 
   const { width, ref } = useResizeDetector()
 
   return (
     <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
-      <Topbar 
-      numPages={numPages as number}
-       setcurrPage={setcurrPage}
+      <Topbar
+        numPages={numPages as number}
+        setcurrPage={setcurrPage}
         currPage={currPage}
-         scale={scale}
-          setScale={setScale}
-          rotation={rotation}
-           setRotation={setRotation}
-           fileUrl={url}
-          />
+        scale={scale}
+        setScale={setScale}
+        rotation={rotation}
+        setRotation={setRotation}
+        fileUrl={url}
+      />
       <div className="flex-1 w-full max-h-screen">
         <SimpleBar autoHide={false} className='max-h-[75vh]' >
-        <div className="" ref={ref}>
-          <Document
-            file={url}
-            className={'max-h-full'}
-            onLoadError={() => { toast.error("Error while loading PDF") }}
-            loading={<div className='flex justify-center'><Loader2 className='my-24 h-6 w-4 animate-spin' /></div>}
-            onLoadSuccess={({ numPages }) => { setNumPages(numPages) }}
-          >
-            {/* {width ?
+          <div className="" ref={ref}>
+            <Document
+              file={url}
+              className={'max-h-full'}
+              onLoadError={() => { toast.error("Error while loading PDF") }}
+              loading={<div className='flex justify-center'><Loader2 className='my-24 h-6 w-4 animate-spin' /></div>}
+              onLoadSuccess={({ numPages }) => { setNumPages(numPages) }}
+            >
+              {/* {width ?
               (
                 <Page
                   // width={width}
@@ -57,14 +60,28 @@ const PdfRenderer = ({ url }: { url: string }) => {
                   />
               )
             } */}
-            <Page
-              width={width ? width : 1}
-              pageNumber={currPage}
-              scale={scale}
-              rotate={rotation}
-            />
-          </Document>
-        </div>
+              {isLoading && renderedScale ? <Page
+                width={width ? width : 1}
+                key={"@"+renderedScale}
+                pageNumber={currPage}
+                scale={scale}
+                rotate={rotation}
+              /> : null}
+
+              <Page
+                className={cn(isLoading ? "hidden" : "")}
+                width={width ? width : 1}
+                pageNumber={currPage}
+                scale={scale}
+                rotate={rotation}
+                key={"@" + scale}
+                loading={
+                  <div className='flex justify-center'><Loader2 className='my-24 h-6 w-6 animate-spin' /></div>
+                }
+                onRenderSuccess={()=>setRenderedScale(scale)}
+              />
+            </Document>
+          </div>
         </SimpleBar>
       </div>
     </div>
